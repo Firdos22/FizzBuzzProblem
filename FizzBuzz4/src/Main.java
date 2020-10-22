a1f628016e699b2860906a8c23b86e7b26b0053c
Firdos22 <72314761+Firdos22@users.noreply.github.com>
2020-10-22 17:16:14 +0530

    Not able to return fizz with another approach

---
+++ FizzBuzz4/src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	//print value
+		for(int i=1;i<=100;i++)
+	  {
+        System.out.println(FizzBzz(i));
+	  }
+	}
+//to check conditions (divisible by 3/5/3*5)
+	public static String FizzBzz(int i) {
+	
+		
+		if(i % 15==0)
+			return "fizzbuzz";
+		if(i % 5 == 0)
+			return "buzz";
+		else
+			return Integer.toString(i);
+	
+		
+		
+//		return Integer.toString(i);//returns result in string format
+	
+	}
+}
